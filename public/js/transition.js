06f15c2d32a4e042480327064e40e2e07b5168fc
Rajat Shrivastava <51124175+rajat-0206@users.noreply.github.com>
2020-12-23 00:05:55 +0530

    particle.js error fixed

--- public/js/transition.js
+++ public/js/transition.js
@@ -5,8 +5,13 @@ video.pause();
 
 function play() {
    video.play();
+   document.getElementById("particles-js").style.visibility = "hidden";
+   
    document.querySelector(".text").style.visibility = "hidden";
    setTimeout(function () {
       video.pause();
+      document.getElementById("particles-js").style.visibility = "visible";
+   document.querySelector(".text").style.visibility = "visible";
    }, 2000);
+   
 }
