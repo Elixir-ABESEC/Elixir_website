742e57c98918d98215e53fd48bb1bd9670be1a77
klofaes <vs201100@gmail.com>
2020-12-22 17:18:53 +0530

    Add warpSpeed transition

---
+++ public/js/transition.js
@@ -0,0 +1,12 @@
+let video = document.getElementById("myVideo");
+let btn = document.getElementById("myBtn");
+
+video.pause();
+
+function play() {
+   video.play();
+   document.querySelector(".text").style.visibility = "hidden";
+   setTimeout(function () {
+      video.pause();
+   }, 2000);
+}
