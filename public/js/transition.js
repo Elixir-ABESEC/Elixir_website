80e6861025b4f63283201c73c5b3ca3e53be1bc6
klofaes <vs201100@gmail.com>
2020-12-23 16:07:27 +0530

    Add new cursor

--- public/js/transition.js
+++ public/js/transition.js
@@ -1,3 +1,8 @@
+/****************************************************************************************
+Author - w3schools
+link - https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_fullscreen_video
+*****************************************************************************************/
+
 let video = document.getElementById("myVideo");
 let btn = document.getElementById("myBtn");
 
@@ -8,5 +13,6 @@ function play() {
    document.querySelector(".text").style.visibility = "hidden";
    setTimeout(function () {
       video.pause();
+      // document.getElementById("myVideo").style.visibility = "hidden";
    }, 3900);
 }
